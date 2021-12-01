89d5584b32996a553cbeef906ba846498b1ec0f5
Connor Boyle <connor.bo@gmail.com>
2021-12-01 12:32:45 -0800

    Implement Date Annotation Endpoint

--- server/openapi_server/controllers/text_date_annotation_controller.py
+++ server/openapi_server/controllers/text_date_annotation_controller.py
@@ -1,4 +1,6 @@
 import connexion
+
+from openapi_server.huggingface import huggingFace
 from openapi_server.models.error import Error  # noqa: E501
 from openapi_server.models.text_date_annotation_request import \
     TextDateAnnotationRequest  # noqa: E501
@@ -18,29 +20,31 @@ def create_text_date_annotations():  # noqa: E501
     status = None
     if connexion.request.is_json:
         try:
-            annotation_request = TextDateAnnotationRequest.from_dict(
-                connexion.request.get_json())  # noqa: E501
-            note = annotation_request._note
-            note = note.text
+            annotation_request = TextDateAnnotationRequest.from_dict(connexion.request.get_json())  # noqa: E501
+            note = annotation_request._note  # noqa: E501
             annotations = []
+            name_annotations = huggingFace.get_entities(
+                note.text, ['DATE', 'AGE'])
+            add_date_annotation(annotations, name_annotations)
             res = TextDateAnnotationResponse(annotations)
             status = 200
         except Exception as error:
             status = 500
+            print(error)
             res = Error("Internal error", status, str(error))
     return res, status
 
 
-def add_date_annotation(annotations, matches, date_format):
+def add_date_annotation(annotations, matches):
     """
     Converts matches to TextDateAnnotation objects and adds them to the
     annotations array specified.
     """
     for match in matches:
         annotations.append(TextDateAnnotation(
-            start=match.start(),
-            length=len(match[0]),
-            text=match[0],
-            date_format=date_format,
-            confidence=95.5
+            start=int(match['start']),
+            length=int(match['end']) - int(match['start']),
+            text=match['word'],
+            date_format='YYYY-MM-DD',  # FIXME: This is wrong generally
+            confidence=float(match['score'] * 100)
         ))
